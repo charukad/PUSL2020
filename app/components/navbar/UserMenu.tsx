c28e26bdf8e7eeaf84bd5d77f1f810d08a623f45
Dasun Charuka <140306227+charukad@users.noreply.github.com>
2024-03-12 06:17:14 +0530

    user menu button

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { AiOutlineAim, AiOutlineMenu } from "react-icons/ai";
+
 const UserMenu = () => {
   return (
     <div className="relative">
@@ -8,6 +10,27 @@ const UserMenu = () => {
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           book your bodima
+        </div>
+        <div
+          onClick={() => {}}
+          className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md 
+          transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className=" hidden md:block"></div>
         </div>
       </div>
     </div>

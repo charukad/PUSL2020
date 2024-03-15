9107ed95ab2b88c87b99b1229200b5564307d6c9
Dasun Charuka <140306227+charukad@users.noreply.github.com>
2024-03-15 21:15:54 +0530

    error-fixing

--- app/types/index.ts
+++ app/types/index.ts
@@ -4,16 +4,6 @@ export type SafeListing = Omit<Listing, "createdAt"> & {
   createdAt: string;
 };
 
-export type SafeReservation = Omit<
-  Reservation, 
-  "createdAt" | "startDate" | "endDate" | "listing"
-> & {
-  createdAt: string;
-  startDate: string;
-  endDate: string;
-  listing: SafeListing;
-};
-
 export type SafeUser = Omit<
   User,
   "createdAt" | "updatedAt" | "emailVerified"
